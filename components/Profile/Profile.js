6abf689dd9464c25f6c173948e96080cc4487541
Sheron Perera <enosh.sheron@gmail.com>
2022-06-16 04:41:45 +0530

    add title

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,10 +1,26 @@
+import Head from 'next/head';
 import classes from './Profile.module.scss';
-
 import ProfileCard from './ProfileCard/ProfileCard';
 
 const ProfilePage = (props) => {
+  const firstName = props.profileData?.find(
+    (data) => data.name === 'First Name'
+  ).value;
+
+  const lastName = props.profileData?.find(
+    (data) => data.name === 'Last Name'
+  ).value;
+
   return (
     <div className={classes.Profile}>
+      <Head>
+        <link rel="icon" href="/static/images/commercial_prime_logo.png" />
+        <title>
+          {firstName} {lastName}'
+          {lastName && lastName.charAt(lastName.length - 1) === 's' ? '' : 's'}{' '}
+          Commercial Prime Profile
+        </title>
+      </Head>
       <ProfileCard profileData={props.profileData} />
     </div>
   );
