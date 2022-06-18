7508a2546f286275c55bb715202d24d928191593
Sheron Perera <enosh.sheron@gmail.com>
2022-06-18 07:05:33 +0530

    fix 22

--- components/Profile/ProfileCard/ProfileCard.js
+++ components/Profile/ProfileCard/ProfileCard.js
@@ -178,8 +178,8 @@ const ProfileCard = (props) => {
     window.gapi.client.request({
       method: 'POST',
       path: 'https://people.googleapis.com/v1/people:createContact',
-      // datatype: 'jsonp',
-      // parent: 'me',
+      datatype: 'jsonp',
+      parent: 'me',
       personFields: 'names,emailAddresses,phoneNumbers',
       body: {
         "resourceName": "people/me",
